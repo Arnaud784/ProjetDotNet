3236f0ce0395c4cfc9e8d8040b560e0e928dadac
Arnaud <arnaud.lieveaux@gmail.com>
2022-02-21 12:18:44 +0100

    exemple plot

--- ProjetDotNet/Form1.Designer.cs
+++ ProjetDotNet/Form1.Designer.cs
@@ -31,11 +31,13 @@ namespace ProjetDotNet
         {
             this.components = new System.ComponentModel.Container();
             this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl2 = new ZedGraph.ZedGraphControl();
+            this.zedGraphControl3 = new ZedGraph.ZedGraphControl();
             this.SuspendLayout();
             // 
             // zedGraphControl1
             // 
-            this.zedGraphControl1.Location = new System.Drawing.Point(37, 36);
+            this.zedGraphControl1.Location = new System.Drawing.Point(13, 13);
             this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.zedGraphControl1.Name = "zedGraphControl1";
             this.zedGraphControl1.ScrollGrace = 0D;
@@ -45,15 +47,49 @@ namespace ProjetDotNet
             this.zedGraphControl1.ScrollMinX = 0D;
             this.zedGraphControl1.ScrollMinY = 0D;
             this.zedGraphControl1.ScrollMinY2 = 0D;
-            this.zedGraphControl1.Size = new System.Drawing.Size(987, 495);
+            this.zedGraphControl1.Size = new System.Drawing.Size(501, 641);
             this.zedGraphControl1.TabIndex = 0;
             this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // zedGraphControl2
+            // 
+            this.zedGraphControl2.Location = new System.Drawing.Point(522, 13);
+            this.zedGraphControl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl2.Name = "zedGraphControl2";
+            this.zedGraphControl2.ScrollGrace = 0D;
+            this.zedGraphControl2.ScrollMaxX = 0D;
+            this.zedGraphControl2.ScrollMaxY = 0D;
+            this.zedGraphControl2.ScrollMaxY2 = 0D;
+            this.zedGraphControl2.ScrollMinX = 0D;
+            this.zedGraphControl2.ScrollMinY = 0D;
+            this.zedGraphControl2.ScrollMinY2 = 0D;
+            this.zedGraphControl2.Size = new System.Drawing.Size(544, 641);
+            this.zedGraphControl2.TabIndex = 1;
+            this.zedGraphControl2.UseExtendedPrintDialog = true;
+            // 
+            // zedGraphControl3
+            // 
+            this.zedGraphControl3.Location = new System.Drawing.Point(1074, 13);
+            this.zedGraphControl3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl3.Name = "zedGraphControl3";
+            this.zedGraphControl3.ScrollGrace = 0D;
+            this.zedGraphControl3.ScrollMaxX = 0D;
+            this.zedGraphControl3.ScrollMaxY = 0D;
+            this.zedGraphControl3.ScrollMaxY2 = 0D;
+            this.zedGraphControl3.ScrollMinX = 0D;
+            this.zedGraphControl3.ScrollMinY = 0D;
+            this.zedGraphControl3.ScrollMinY2 = 0D;
+            this.zedGraphControl3.Size = new System.Drawing.Size(538, 641);
+            this.zedGraphControl3.TabIndex = 2;
+            this.zedGraphControl3.UseExtendedPrintDialog = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1068, 565);
+            this.ClientSize = new System.Drawing.Size(1625, 836);
+            this.Controls.Add(this.zedGraphControl3);
+            this.Controls.Add(this.zedGraphControl2);
             this.Controls.Add(this.zedGraphControl1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -64,6 +100,8 @@ namespace ProjetDotNet
         #endregion
 
         private ZedGraph.ZedGraphControl zedGraphControl1;
+        private ZedGraph.ZedGraphControl zedGraphControl2;
+        private ZedGraph.ZedGraphControl zedGraphControl3;
     }
 }
 
--- ProjetDotNet/Form1.cs
+++ ProjetDotNet/Form1.cs
@@ -12,9 +12,109 @@ namespace ProjetDotNet
 {
     public partial class Form1 : Form
     {
+        private Random rand = new Random(0);
+        private double[] RandomWalk(int points = 5, double start = 100, double mult = 50)
+        {
+            // return an array of difting random numbers
+            double[] values = new double[points];
+            values[0] = start;
+            for (int i = 1; i < points; i++)
+                values[i] = values[i - 1] + (rand.NextDouble() - .5) * mult;
+            return values;
+        }
         public Form1()
         {
             InitializeComponent();
+            plotBar();
+            plotScatter();
+            plotLines();
+        }
+
+        public void plotBar()
+        {
+            // generate some random Y data
+            int pointCount = 5;
+            double[] xs = RandomWalk(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // clear old curves
+            zedGraphControl1.GraphPane.CurveList.Clear();
+
+            // plot the data as bars
+            zedGraphControl1.GraphPane.AddBar("Group A", xs, ys1, Color.Blue);
+            zedGraphControl1.GraphPane.AddBar("Group B", xs, ys2, Color.Red);
+
+            // style the plot
+            zedGraphControl1.GraphPane.Title.Text = $"Bar Plot ({pointCount:n0} points)";
+            zedGraphControl1.GraphPane.XAxis.Title.Text = "Horizontal Axis Label";
+            zedGraphControl1.GraphPane.YAxis.Title.Text = "Vertical Axis Label";
+
+            // auto-axis and update the display
+            zedGraphControl1.GraphPane.XAxis.ResetAutoScale(zedGraphControl1.GraphPane, CreateGraphics());
+            zedGraphControl1.GraphPane.YAxis.ResetAutoScale(zedGraphControl1.GraphPane, CreateGraphics());
+            zedGraphControl1.Refresh();
+        }
+
+        public void plotScatter()
+        {
+            // generate some random Y data
+            int pointCount = 100;
+            double[] xs1 = RandomWalk(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] xs2 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // clear old curves
+            zedGraphControl2.GraphPane.CurveList.Clear();
+
+            // plot the data as curves
+            var curve1 = zedGraphControl2.GraphPane.AddCurve("Series A", xs1, ys1, Color.Blue);
+            curve1.Line.IsAntiAlias = true;
+
+            var curve2 = zedGraphControl2.GraphPane.AddCurve("Series B", xs2, ys2, Color.Red);
+            curve2.Line.IsAntiAlias = true;
+
+            // style the plot
+            zedGraphControl2.GraphPane.Title.Text = $"Scatter Plot ({pointCount:n0} points)";
+            zedGraphControl2.GraphPane.XAxis.Title.Text = "Horizontal Axis Label";
+            zedGraphControl2.GraphPane.YAxis.Title.Text = "Vertical Axis Label";
+
+            // auto-axis and update the display
+            zedGraphControl2.GraphPane.XAxis.ResetAutoScale(zedGraphControl2.GraphPane, CreateGraphics());
+            zedGraphControl2.GraphPane.YAxis.ResetAutoScale(zedGraphControl2.GraphPane, CreateGraphics());
+            zedGraphControl2.Refresh();
+        }
+
+        public void plotLines()
+        {
+            // generate some random Y data
+            int pointCount = 100_000;
+            double[] xs = RandomWalk(pointCount);
+            double[] ys1 = RandomWalk(pointCount);
+            double[] ys2 = RandomWalk(pointCount);
+
+            // clear old curves
+            zedGraphControl3.GraphPane.CurveList.Clear();
+
+            // plot the data as curves
+            var curve1 = zedGraphControl3.GraphPane.AddCurve("Series A", xs, ys1, Color.Blue);
+            curve1.Line.IsAntiAlias = true;
+            curve1.Symbol.IsVisible = false;
+
+            var curve2 = zedGraphControl3.GraphPane.AddCurve("Series B", xs, ys2, Color.Red);
+            curve2.Line.IsAntiAlias = true;
+            curve2.Symbol.IsVisible = false;
+
+            // style the plot
+            zedGraphControl3.GraphPane.Title.Text = $"Scatter Plot ({pointCount:n0} points)";
+            zedGraphControl3.GraphPane.XAxis.Title.Text = "Horizontal Axis Label";
+            zedGraphControl3.GraphPane.YAxis.Title.Text = "Vertical Axis Label";
+
+            // auto-axis and update the display
+            zedGraphControl3.GraphPane.XAxis.ResetAutoScale(zedGraphControl3.GraphPane, CreateGraphics());
+            zedGraphControl3.GraphPane.YAxis.ResetAutoScale(zedGraphControl3.GraphPane, CreateGraphics());
+            zedGraphControl3.Refresh();
         }
     }
 }
